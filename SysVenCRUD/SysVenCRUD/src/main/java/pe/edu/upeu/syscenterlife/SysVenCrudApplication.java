24e5719c9ee4a771ffdead31dac40d04ec4f3a5a
AlanLuque24 <luquecalcinaalanfrancisco9@gmail.com>
2024-05-09 21:59:07 -0500

    Add files via upload
    mi proyecto

---
+++ SysVenCRUD/SysVenCRUD/src/main/java/pe/edu/upeu/syscenterlife/SysVenCrudApplication.java
@@ -0,0 +1,24 @@
+<<<<<<<< HEAD:SysListasCRUD/src/main/java/pe/edu/upeu/SysListasCRUD/SysListasCrudApplication.java
+package pe.edu.upeu.SysListasCRUD;
+========
+package pe.edu.upeu.syscenterlife;
+>>>>>>>> be58d28738f20add59528b260efb3df5ebb2d48b:SysVenCRUD/SysVenCRUD/src/main/java/pe/edu/upeu/syscenterlife/SysVenCrudApplication.java
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+<<<<<<<< HEAD:SysListasCRUD/src/main/java/pe/edu/upeu/SysListasCRUD/SysListasCrudApplication.java
+public class SysListasCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysListasCrudApplication.class, args);
+========
+public class SysVenCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysVenCrudApplication.class, args);
+>>>>>>>> be58d28738f20add59528b260efb3df5ebb2d48b:SysVenCRUD/SysVenCRUD/src/main/java/pe/edu/upeu/syscenterlife/SysVenCrudApplication.java
+	}
+
+}
---
+++ SysVenCRUD/SysVenCRUD/src/test/java/pe/edu/upeu/syscenterlife/SysVenCrudApplicationTests.java
@@ -0,0 +1,21 @@
+<<<<<<<< HEAD:SysListasCRUD/src/test/java/pe/edu/upeu/SysListasCRUD/SysListasCrudApplicationTests.java
+package pe.edu.upeu.SysListasCRUD;
+========
+package pe.edu.upeu.syscenterlife;
+>>>>>>>> be58d28738f20add59528b260efb3df5ebb2d48b:SysVenCRUD/SysVenCRUD/src/test/java/pe/edu/upeu/syscenterlife/SysVenCrudApplicationTests.java
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+<<<<<<<< HEAD:SysListasCRUD/src/test/java/pe/edu/upeu/SysListasCRUD/SysListasCrudApplicationTests.java
+class SysListasCrudApplicationTests {
+========
+class SysVenCrudApplicationTests {
+>>>>>>>> be58d28738f20add59528b260efb3df5ebb2d48b:SysVenCRUD/SysVenCRUD/src/test/java/pe/edu/upeu/syscenterlife/SysVenCrudApplicationTests.java
+
+	@Test
+	void contextLoads() {
+	}
+
+}
